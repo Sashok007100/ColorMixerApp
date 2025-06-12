416e4ecca2657ded5ea30c8d67c12f7bed5602ea
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-12 20:30:20 +0300

    Был добавлено расширение для UIColor, чтобы получать значения цветов RGB.

--- ColorMixerApp/ViewController.swift
+++ ColorMixerApp/ViewController.swift
@@ -74,3 +74,17 @@ extension Float {
     }
 }
 
+extension UIColor {
+    func getColors() -> (red: CGFloat, green: CGFloat, blue: CGFloat)? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if self.getRed(&red, green: &green, blue: &blue, alpha: nil) {
+            return (red, green, blue)
+        } else {
+            return nil
+        }
+    }
+}
+
