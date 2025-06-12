e02a7adcefe13688abb5c8b4602c2e35eaaa10f1
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-12 19:19:18 +0300

    Был создан класс для стартового экрана приложения.

---
+++ ColorMixerApp/StartScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartScreenViewController.swift
+//  ColorMixerApp
+//
+//  Created by Alexandr Artemov (Mac Mini) on 12.06.2025.
+//
+
+import UIKit
+
+final class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
